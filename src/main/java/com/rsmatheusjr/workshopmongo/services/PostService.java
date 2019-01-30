5cde935524aedcf5286c812547d5414ff386c89e
Roberto Matheus <rsmatheusjr@hotmail.com>
2019-01-30 02:47:13 -0200

    Obtendo um post por id

---
+++ src/main/java/com/rsmatheusjr/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.rsmatheusjr.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.rsmatheusjr.workshopmongo.domain.Post;
+import com.rsmatheusjr.workshopmongo.repository.PostRepository;
+import com.rsmatheusjr.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+}
