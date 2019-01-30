abcffc264d18ee0963e222459bb7d8103960bdc9
Roberto Matheus <rsmatheusjr@hotmail.com>
2019-01-30 02:26:16 -0200

    Referenciando os posts dos usuarios

--- src/main/java/com/rsmatheusjr/workshopmongo/config/Instantiation.java
+++ src/main/java/com/rsmatheusjr/workshopmongo/config/Instantiation.java
@@ -42,9 +42,13 @@ public class Instantiation implements CommandLineRunner {
 		Post post1 = new Post(null,sdf.parse("21/03/2018"),"Partiu viagem","Vou viajar para sao paulo, abçs", new AuthorDTO(maria));
 		Post post2 = new Post(null,sdf.parse("23/03/2018"),"Bom dia","Acordei feliz hj", new AuthorDTO(maria));
 		
-
-		
 		postRepository.saveAll(Arrays.asList(post1, post2));
+		
+		
+		maria.getPosts().addAll(Arrays.asList(post1,post2));
+		userReposiroty.save(maria);
+		
+		
 	}
 
 }
